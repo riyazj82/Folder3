3023714ace3a2388bcb8ec9d14dc9266d7032222
Riyaz Jaleel <64475310+riyazj82@users.noreply.github.com>
2021-12-06 23:23:09 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_canonical_user_id" "current_user" {}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "mybucket"
+
+  grant {
+    id          = data.aws_canonical_user_id.current_user.id
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://mybucket.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+  grant {
+    type        = "Group"
+    permissions = ["READ_ACP", "WRITE"]
+    uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+  }
+
+
+resource "aws_iam_saml_provider" "default" {
+  name                   = "my-saml-provider"
+  saml_metadata_document = file("saml-metadata.xml")
+}
+
+resource "aws_cognito_identity_pool" "main" {
+  identity_pool_name               = "identity pool"
+  allow_unauthenticated_identities = false
+  allow_classic_flow               = false
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  
+  policy = jsonencode({
+      "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Action": [
+            "s3:ListBucket"
+         ],
+         "Resource": [
+            "arn:aws:s3:::BUCKET_NAME"
+         ]
+      }
+   ]
+}
+  )
+
+  resource "aws_iam_role_policy" "unauthenticated" {
+  name = "unauthenticated_policy"
+  role = aws_iam_role.authenticated.id
+  policy=aws_iam_policy.policy
+  }
